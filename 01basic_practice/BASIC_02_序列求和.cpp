ac946b28a9d94b44e70b3e5f25aa5e8af13efecd
r8pto <1726284081@qq.com>
2023-03-30 22:10:53 +0800

    20230330

--- 01basic_practice/BASIC_02_序列求和.cpp
+++ 01basic_practice/BASIC_02_序列求和.cpp
@@ -4,15 +4,12 @@ using namespace std;
 int main(){
     int n;
     std::cin >> n;
-    if(1<n<200){
-        int sum = 0;
-        for (int i = 1;i <= n;i++){
-            sum = i + sum;
-        }
-        cout<<sum<<endl;
-    }else{
-        std::cout << "请输入范围在1<n<200的数"<<endl;
+    long long sum = 0;
+    for (int i = 1;i <= n;i++){
+        sum = i + sum;
     }
+    cout<<sum<<endl;
+
 
     return 0;
 }
