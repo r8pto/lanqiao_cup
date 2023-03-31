8754f426bf33faa30e09613fcd5cc5f8ef373a11
r8pto <1726284081@qq.com>
2023-03-31 21:59:20 +0800

    20230331

--- 01basic_practice/tempCodeRunnerFile.cpp
+++ 01basic_practice/tempCodeRunnerFile.cpp
@@ -1,28 +1,8 @@
-#include<iostream>
-#include<stdio.h>
-#include<cmath>
-#define MAX 32
-using namespace std;
-
-int main(){
-    // 1. 输入一个非负整数a
-    int a,i;
-    scanf("%d", &a);
-    string str = "0123456789ABCDE";
-    int n = -1;
-    int arr[MAX], temp;
-    for(i=0;i<8;i++){
-        int b = pow(16,i);
-        temp = a/b;
-        arr[i] = temp % 16;
-        n+=1;
-        if(temp==0){
-            break;
-        }
-        
-    }
-    for(int j= n-1;j>=0;j--){
-        int k = arr[j];
-        cout << str[k];
-    }
-}
+    // while(s3[1]!=0){
+    //     k = s3[1]%8;
+    //     s7[b++]=s5[k];
+    //     s3[1]=s3[1]/8;			
+    // }
+    // for(j = b-1;j>=0;j--){
+    //     cout << s7[j];
+    // }
